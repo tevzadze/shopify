3f7ba59c0aa3120c7e650849a19e47174cede88f
Davyd Tevzadze <pawtod22@gmail.com>
2023-08-16 22:25:33 +0300

    Setup CSS

--- remix.config.js
+++ remix.config.js
@@ -16,6 +16,7 @@ module.exports = {
   serverModuleFormat: 'esm',
   serverPlatform: 'neutral',
   serverMinify: process.env.NODE_ENV === 'production',
+  postcss: true,
   future: {
     v2_meta: true,
     v2_headers: true,
